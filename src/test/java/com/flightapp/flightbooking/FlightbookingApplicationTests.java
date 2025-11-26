4867a2e05b96577759eea61781e3371848350263
shrest21 <shrest21@gmail.com>
2025-11-26 22:15:25 +0530

    Added basic Spring Boot test class

---
+++ src/test/java/com/flightapp/flightbooking/FlightbookingApplicationTests.java
@@ -0,0 +1,12 @@
+package com.flightapp.flightbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightbookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
